31d7dd830a79aa8d812c03204536f3f955ab87d8
Moin Peer <moinpeer321@gmail.com>
2024-12-02 23:15:57 +0530

    Basic number examples

---
+++ number_python.py
@@ -0,0 +1,35 @@
+# single line comment
+"""multiple
+line
+comment """
+
+# numbers follows pedmas : (),**,%,/,//,*,+,-
+# data type : int, float, complex, factions
+
+import math
+number = -10.9294
+
+print(math.floor(number)) #integer
+print(math.ceil(number)) #greater integer
+print(round(number))
+print(round(number,2))
+print("..............maths operator ...............")
+a = 8.1
+b = 8
+
+print(a/b)
+print(a//b)
+print(a*b)
+print(a+b)
+print(a-b)
+print(a**b)
+print(a%b)
+
+print(".............. convert float and int ...............")
+
+integer = 13
+floats = 12.5
+
+print(int(floats))
+print(float(integer))
+
